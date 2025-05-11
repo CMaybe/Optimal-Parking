fb8d8313fec30e99bca0a87b51b84f260aa48f5c
CMaybe <asbsds123@gmail.com>
2025-05-11 11:54:51 +0000

    Refactor: Simplify SystemModel class by removing unused parameters and updating matrix names for clarity

--- optimal_parking/include/optimal_parking/system/system_model.hpp
+++ optimal_parking/include/optimal_parking/system/system_model.hpp
@@ -10,44 +10,28 @@
 namespace optimal_parking {
 
 struct ModelMatrices {
-    Eigen::Matrix<double, 5, 5> A;
-    Eigen::Matrix<double, 5, 2> B;
-    Eigen::Matrix<double, 5, 1> g;
+    Eigen::Matrix<double, 5, 5> Ad;
+    Eigen::Matrix<double, 5, 2> Bd;
+    Eigen::Matrix<double, 5, 1> gd;
 };
 
 class SystemModel {
 public:
     SystemModel() = default;
-    SystemModel(const double& dt, const double& wheel_radius, const double& car_length, const double& car_width);
+    SystemModel(const double& car_length, const double& car_width);
     SystemModel(const std::string& path);
     SystemModel(const SystemModel& other);
     void initialize(const std::string& path);
-    void initialize(const double& dt, const double& wheel_radius, const double& car_length, const double& car_width);
+    void initialize(const double& car_length, const double& car_width);
 
     Eigen::Vector<double, 5> f(const SystemState& state, const SystemInput& input) const;
-    ModelMatrices getSystemJacobian(const SystemState& state, const SystemInput& input);
-    inline Eigen::Matrix<double, 5, 5> Ac() const { return Ac_; };
-    inline Eigen::Matrix<double, 5, 5> Ad() const { return Ad_; };
-    inline Eigen::Matrix<double, 5, 2> Bc() const { return Bc_; };
-    inline Eigen::Matrix<double, 5, 2> Bd() const { return Bd_; };
-    inline double dt() const { return dt_; };
-    inline double wheel_radius() const { return wheel_radius_; };
+    ModelMatrices getSystemJacobian(const SystemState& state, const SystemInput& input, const double& dt) const;
     inline double car_length() const { return car_length_; };
     inline double car_width() const { return car_width_; };
 
 private:
-    double dt_;
-    double wheel_radius_;
     double car_length_;
     double car_width_;
-
-    Eigen::Matrix<double, 5, 5> Ac_;
-    Eigen::Matrix<double, 5, 2> Bc_;
-    Eigen::Matrix<double, 5, 1> gc_;
-
-    Eigen::Matrix<double, 5, 5> Ad_;
-    Eigen::Matrix<double, 5, 2> Bd_;
-    Eigen::Matrix<double, 5, 1> gd_;
 };
 
 }  // namespace optimal_parking
